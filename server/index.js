2f9f82eaa65c9132c4d9e19897e9286062b6ce98
Uros Spasojevic <uspasojevic96@gmail.com>
2016-11-28 14:13:38 +0100

    initial commit

---
+++ server/index.js
@@ -0,0 +1,84 @@
+// Init express
+
+var express = require('express');
+var app = express();
+
+app.set("port", process.env.PORT || 3000);
+
+// Parse JSON Body
+
+var bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
+// Add websocket support
+
+var expressWs = require('express-ws')(app);
+
+// Subscriptions
+
+var subscriptions = {};
+var keys = [];
+
+// Gate for client
+
+app.ws('/gate', function(ws, req) {
+	ws.on('message', function(msg) {
+		msg = JSON.parse(msg);
+
+		if(msg.message == "subscribe") {
+			if(subscriptions[msg.key] == undefined) {
+				subscriptions[msg.key] = ws;
+				keys = Object.keys(subscriptions);
+			}
+		}
+
+		if(msg.message == "unsubscribe") {
+			if(keys.indexOf(msg.key) != -1) {
+				subscriptions[msg.key].close();
+				delete subscriptions[msg.key];
+				keys = Object.keys(subscriptions);
+			}
+		}
+	});
+});
+
+// Gate for Tasker
+
+app.post('/', function (req, res) {
+	var keys = req.body.key.split("|");
+
+	var sentTo = [];
+
+	keys.forEach(function(key){
+		if(subscriptions[key] == undefined){
+			subscriptions[key] = [];
+		}
+
+		subscriptions[key] = subscriptions[key].filter(function (ws){
+			try{
+				if(sentTo.indexOf(ws) == -1){
+					ws.send(JSON.stringify(req.body));
+
+					sentTo.push(ws);
+				}
+				else
+				{
+					ws.send(JSON.stringify({action: "_ping"}));
+				}
+
+				return true;
+			} catch(e){
+				return false;
+			}
+		});
+	});
+
+	res.send({status: "ok", sentTo: sentTo.length});
+});
+
+// Listen
+
+app.listen(app.get("port"), function () {
+	console.log('ws-net-control is listening on '+app.get("port")+"!");
+});
+
